16fc61a92995292ce7d94d9385a62a555fb131ab
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-06-12 10:43:06 +0600

    connect to MongoDB using environment variables

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // App initialization
 const app = express();
@@ -15,6 +16,26 @@ dotenv.config();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 // Root route
 app.get("/", (req, res) => {
