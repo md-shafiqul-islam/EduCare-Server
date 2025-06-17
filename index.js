f2a5ea86aeb1576e2c3129d43a96330e72f1cb2d
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-06-17 12:34:51 +0600

    deploy API to Vercel

--- index.js
+++ index.js
@@ -139,8 +139,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
   }
